0086769f6ad08b2cbc9dd9e813396f1cdde9f5a6
yongdoree <yongdoree@gmail.com>
2016-03-26 01:25:16 +0900

    update: added django_extensions, debug_toolbar to settings.py

--- goqual/goqual/settings.py
+++ goqual/goqual/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'debug_toolbar',
+    'django_extensions',
 ]
 
 MIDDLEWARE_CLASSES = [
